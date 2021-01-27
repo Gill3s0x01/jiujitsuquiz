056b67739e64b4e2a5db109ccb0f148e9470c644
root <root@GILLES.localdomain>
2021-01-26 22:45:43 -0300

    Second day of Alura's Nextjs immersion completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
-import db from '../../db.json'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import Link from  'next/link'
+
+
 
 import Widget from '../components/Widget'
 import QuizLogo from '../components/QuizLogo'
-import QuizBackground from '../components/QuizBackground'
 import Footer from '../components/Footer'
+import QuizBackground from '../components/QuizBackground'
 import GitHubCorner from '../components/GitHubCorner'
 import QuizContainer from '../components/QuizContainer'
 
+import db from '../../db.json'
+
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
@@ -18,12 +27,33 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
+            <form onSubmit={(e) => {
+              e.preventDefault();
+              router.push(`/quiz?name=${name}`)
+            }}>
+              <input 
+                placeholder="Digite seu nome"
+                value={name}
+                onChange={(e)=> setName(e.target.value)}
+              />
+              <button 
+                type="submit"
+                disabled={name === ''}
+              >
+                Jogar {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
         <Widget>
           <Widget.Content>
             <h1>Quizes da Galera</h1>
+
+            <p>Dá uma olhada nesses quizes incríveis que o pessoal da Imersão Nextjs fez:</p>
+            <Link href="https://aluraquiz-base-git-main.alura-challenges.vercel.app">
+              <a>The legend of zelda</a>
+            </Link>
 
             <p>lorem ipsum dolor sit amet...</p>
           </Widget.Content>
